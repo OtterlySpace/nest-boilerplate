4cd14ca8ba786b7b4b19c6e447b0b5d001296fef
Alexandre Sanchez <alex73630@gmail.com>
2020-10-20 19:53:15 +0200

    Added environment variable support for migrations

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,13 @@
+import dotenv from "dotenv"
+dotenv.config()
+
 module.exports = {
 	type: "postgres",
-	host: "postgres-database",
-	port: 5432,
-	username: "boilerplate-postgres",
-	password: "BadPassword",
-	database: "boilerplateapi",
+	host: process.env.POSTGRES_HOST,
+	port: process.env.POSTGRES_PORT,
+	username: process.env.POSTGRES_USER,
+	password: process.env.POSTGRES_PASSWORD,
+	database: process.env.POSTGRES_DB,
 	entities: ["dist/**/*.entity.js"],
 	migrations: ["dist/migrations/*.js"],
 	cli: {
